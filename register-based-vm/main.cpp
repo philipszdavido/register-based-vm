4f0877030d5896588d89fb7a86916e04e312e7e5
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-01-20 21:05:44 +0100

    Initial Commit

---
+++ register-based-vm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  register-based-vm
+//
+//  Created by Chidume Nnamdi on 20/01/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
